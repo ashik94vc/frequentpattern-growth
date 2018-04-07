57471b862a7b0f14a8560ebbec734949a0267dfa
Ashik Vetrivelu <vcashik@gmail.com>
2018-04-06 20:37:22 -0400

    Made environment and code changes    * added git ignore    * Fixed bugs in tree    * completed FPTree construction

--- lib/fptree.py
+++ lib/fptree.py
@@ -1,7 +1,27 @@
-from pandas import DataFrame
+from lib.node import Node
+from lib.table import Table
+from lib.tree import Tree
 
-def constructHeader(min_support=0):
-    header = df.stack().value_counts().to_frame()
-    header = header.drop(header[header[0] < min_support].index)
-    order = list(header.T.columns.values)
-    return header.T
+class FPtree(object):
+
+    def __init__(self,dataframe, min_support):
+        self.df = dataframe
+        self.min_support = min_support
+        support_table = self.df.value_counts()
+        filtered_table = {k:v for k,v in support_table.items() if v > self.min_support}
+        filtered_table = dict(sorted(filtered_table.items(),key=lambda x:x[1], reverse=True))
+        filtered_values = list(filtered_table.keys())
+        header_table = self.df.row_store
+        for idx,row in enumerate(header_table):
+            new_row = list(filter(lambda x: x in filtered_values, row))
+            new_row = new_row.sort(key=lambda x: filtered_values.index(x))
+            header_table[idx] = new_row
+
+        self.fptree = Tree()
+
+        for row in header_table:
+            row_tree = Tree()
+            for value in row:
+                node = Tree(Node(value, 1))
+                row_tree = row_tree.addChild(node)
+            self.fptree.mergeTree(row_tree)
--- lib/tree.py
+++ lib/tree.py
@@ -1,14 +1,63 @@
+from node import Node
 class Tree(object):
 
-    def __init__(self, node=None, children=list()):
-        self.node = None
-        self.children = children
+    def __init__(self, item=None, support=None):
+        self.item = item
+        self.support = support
+        self.parent = None
+        self.children = list()
+
+    def __contains__(self,other):
+        for child in self.getChildren():
+            if child == other:
+                return True
+        return False
+
+    def __repr__(self, level=0):
+        ret = "\t"*level+str(self.item)+"\n"
+        for child in self.children:
+            ret += child.__repr__(level+1)
+        return ret
+
+    def __eq__(self, other):
+        assert isinstance(other,Tree), "Can only compare with objects of same type"
+        if other.item == self.item:
+            return True
+        else:
+            return False
 
     def addChild(self, child):
         assert isinstance(child,Tree), "Child should be of type %r" % type(self)
+        child.parent = self
         self.children.append(child)
+        return self.children[-1]
+
+    def getChildren(self):
+        return self.children
+
+    def getChild(self,childIndex):
+        return self.children[childIndex]
 
     def addChildren(self, children):
         assert isinstance(children, list), "Children should be of type list found %r" % type(children)
         assert all(isinstance(child,Tree) for child in children), "Children should be a list of Tree, found a list of %r" %type(children[0])
+        for child in children:
+            child.parent = self
         self.children.extend(children)
+
+    def childIndex(self, child):
+        for idx,chi in enumerate(self.children):
+            if chi == child:
+                return idx
+        return -1
+
+    def mergeTree(self, tree):
+        if self == tree:
+            for child in tree.children:
+                if child in self:
+                    id = self.childIndex(child)
+                    self_child = self.getChild(id)
+                    self_child.support += child.support
+                    self_child.mergeTree(child)
+                else:
+                    self.addChild(child)
